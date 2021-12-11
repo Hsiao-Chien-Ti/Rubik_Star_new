0c71550020a1552add5b29ff212e935718d009c7
Hsiao-Chien-Ti <0410adi@gmail.com>
2021-12-12 02:32:55 +0800

    level 3 unlock&levelup/asteroid drop

--- Assets/Script/AsteroidDrop.cs
+++ Assets/Script/AsteroidDrop.cs
@@ -8,15 +8,21 @@ public class AsteroidDrop : MonoBehaviour
     public AudioSource Audio;
     public AudioClip dropAudio;
     public AudioClip boomAudio;
+    public List<Vector3> posList;
     float dropTime;
     bool dropFlag=false;
     public int dropMin;
     public int dropMax;
     float timeStamp;
     bool started = false;
+    GameObject aim;
+    public GameObject aimPrefab;
+    Transform t;
     private void Start()
     {
+        posList= new List<Vector3> { new Vector3(-1, 1.51f, 1), new Vector3(0, 1.51f, 1), new Vector3(1, 1.51f, 1), new Vector3(-1, 1.51f, 0), new Vector3(0, 1.51f, 0), new Vector3(1, 1.51f, 0), new Vector3(1, 1.51f, -1), new Vector3(0, 1.51f, -1), new Vector3(1, 1.51f, -1) };
         dropTime = Random.Range(dropMin, dropMax);
+        
     }
     private void FixedUpdate()
     {
@@ -34,7 +40,12 @@ public class AsteroidDrop : MonoBehaviour
     }
     public void fly()
     {
-        rb.AddForce(Random.Range(-50, 50), 0, Random.Range(-50, 50));
+        int posidx = Random.Range(0, 9);
+        Vector3 initpos = posList[posidx]+new Vector3(0, 11, 0);
+        rb.position = initpos;
+        transform.position=Vector3.MoveTowards(transform.position, posList[posidx],5);
+        aim = GameObject.Instantiate(aimPrefab, posList[posidx],Quaternion.Euler(90,0,0));
+        //rb.AddForce(Random.Range(-50, 50), 0, Random.Range(-50, 50));
         Audio.volume = 1.0f;
         Audio.clip = dropAudio;
         Audio.Play();
@@ -45,6 +56,7 @@ public class AsteroidDrop : MonoBehaviour
         Audio.clip = boomAudio;
         Audio.volume = 0.1f;
         Audio.Play();
+        Destroy(aim);
         Audio.SetScheduledEndTime(AudioSettings.dspTime + 1f);
         GetComponent<Fracture>().FractureObject();
     }
