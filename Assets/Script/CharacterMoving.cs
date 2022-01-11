40f80ccf5c65d1c040571db6d4e372ff3d2c5537
Hsiao-Chien-Ti <0410adi@gmail.com>
2022-01-11 16:55:44 +0800

    fix wall collision(change character box collider physics material into bounce)/Heading to the Final Game Competition~~~HURRAYgit add .git add .git add .git add .git add .

--- Assets/Script/CharacterMoving.cs
+++ Assets/Script/CharacterMoving.cs
@@ -7,12 +7,9 @@ public class CharacterMoving : MonoBehaviour
     public Animator anim;
     float speed = 3f;
     float rotationSpeed = 700f;
-    float gravity = 100;
-    bool onGround = false;
     private Rigidbody rb;
-    float distanceToGround;
-    Vector3 Groundnormal;
     public CubeState cubeState;
+    Vector3 movement = Vector3.zero;
 
     private void Start()
     {
@@ -21,10 +18,10 @@ public class CharacterMoving : MonoBehaviour
         rb.freezeRotation = true;
         
     }
-    private void FixedUpdate()
+    private void Update()
     {
-        Vector3 movement=Vector3.zero;
-        if(cubeState.gameObject.GetComponent<RotateBigCube>().rubik==1)
+        movement = Vector3.zero;
+        if (cubeState.gameObject.GetComponent<RotateBigCube>().rubik==1)
         {
             if((transform.up-Vector3.up).magnitude<0.05)
             {
@@ -55,9 +52,11 @@ public class CharacterMoving : MonoBehaviour
                 movement = new Vector3(-Input.GetAxis("Horizontal"), 0, -Input.GetAxis("Vertical"));
             }
         }
-
-
-
+    }
+    private void FixedUpdate()
+    {
+        //Vector3 movement=Vector3.zero;
+        Quaternion toRotation;
         float magnitude = Mathf.Clamp01(movement.magnitude) * speed;
         movement.Normalize();
         if (cubeState.autoRotating)
@@ -73,21 +72,15 @@ public class CharacterMoving : MonoBehaviour
             rb.velocity = Vector3.zero;
             anim.SetBool("IsRunning", false);
         }
-        //charCon.SimpleMove(movement * magnitude);
-        Debug.DrawRay(transform.position, transform.position + 100 * transform.forward);
-        //print(movement);
-        //print(Vector3.Project(movement, transform.forward));
-        //if (!GetComponent<NowPosition>().isGround)
-        //{
-        //    movement = movement - Vector3.Project(movement, transform.forward);
-        //}
         rb.MovePosition(transform.position + movement * Time.deltaTime * magnitude);
         
         if (movement != Vector3.zero)
         {
-            Quaternion toRotation = Quaternion.LookRotation(movement, transform.up);
+            toRotation = Quaternion.LookRotation(movement);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
         }
+        
+
     }
     private void OnCollisionEnter(Collision collision)
     {
